1fbdc7b8fcde33868b3075302de1bec6a84afd72
Tadeas Petak <tadeaspetak@gmail.com>
2021-02-02 17:10:27 +0100

    fix: lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Tune, getStrokes, Parsed } from "../lib";
 import { useInterval } from "../lib/useInterval";
